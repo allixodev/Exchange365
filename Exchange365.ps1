6e7efc9adfa1ca96f302fd6534b0d8e3a9d98338
Gary Gagnon <ggagnon@allixo.com>
2018-09-14 16:58:59 -0700

    Added example command file

--- Exchange365.ps1
+++ Exchange365.ps1
@@ -5,41 +5,74 @@ function PresentExchangeOptions {
     Write-Host "2. Edit Mailbox"
     Write-Host "3. View Quarantine"
     Write-Host "4. Release Quarantine"
-    Write-Host "5. Exit"
+    Write-Host "5. Run My Command"
+    Write-Host "6. Import Commands"
+    Write-Host "7. Exit"
 
     $option = Read-Host "What would you like to do?"
 
-    if($option -eq "1") {
-        $user = Read-Host "Office365 Email"
-        Write-Host "=================MAILBOX=================="
-        Get-Mailbox -Identity $user | fl
-        Write-Host "=================STATISTICS=================="
-        $stats = Get-MailboxStatistics $user
-        $stats | fl
-        Write-Host "======SIZE======"
-        $stats | ft displayname, totalitemsize
+    switch($option) {
+        "1" {
+            $user = Read-Host "Office365 Email"
+            Write-Host "=================MAILBOX=================="
+            Get-Mailbox -Identity $user | Format-List
+            Write-Host "=================STATISTICS=================="
+            $stats = Get-MailboxStatistics $user
+            $stats | Format-List
+            Write-Host "======SIZE======"
+            $stats | Format-Table displayname, totalitemsize
+        }
+        "2" {
+            Write-Host "Not implimented..."
+        }
+        "3" {
+            $user = Read-Host "Office365 Email"
+            $startdate = Read-Host "Start Date (01/01/0001)"
+            $enddate = Read-Host "End Date (01/01/0001)"
+            Get-QuarantineMessage -RecipientAddress $user -StartReceivedDate $startdate -EndReceivedDate $enddate | Format-List
+        }
+        "4" {
+            $messageid = Read-Host "Quarantine MessageId"
+            Write-Host "Releasing quarantined message..."
+            Release-QuarantineMessage -Identity $messageid
+        }
+        "5" {
+            Write-Host "Check Here For Commands: https://docs.microsoft.com/en-us/powershell/module/exchange/mailboxes/get-mailbox?view=exchange-ps"
+            $command = Read-Host "Command"
+            Write-Host "==========COMMAND RESULTS============"
+            try {
+                Invoke-Expression $command
+            }
+            catch {
+                Write-Host "Error invoking your command..."
+                $_
+            }
+        }
+        "6" {
+            $file = Read-Host "Command File Location"
+            $commands = [IO.File]::ReadAllText($file)
+            Write-Host "===========IMPORTED COMMANDS============"
+            $commands
+            Write-Host "===========COMMAND RESULTS=============="
+            try {
+                Invoke-Expression $commands
+            }
+            catch {
+                Write-Host "Error invoking commands..."
+                $_
+            }
+        }
     }
-    if($option -eq "2") {
-        Write-Host "Not implimented"
-    }
-    if($option -eq "3") {
-        $user = Read-Host "Office365 Email"
-        $startdate = Read-Host "Start Date (01/01/0001)"
-        $enddate = Read-Host "End Date (01/01/0001)"
-        Get-QuarantineMessage -RecipientAddress $user -StartReceivedDate $startdate -EndReceivedDate $enddate | fl
-    }
-    if($option -eq "4") {
-        $messageid = Read-Host "Quarantine MessageId"
-        Write-Host "Releasing quarantined message..."
-        Release-QuarantineMessage -Identity $messageid
-    }
-    if($option -ne "5") {
+
+    if($option -ne "7") {
         PresentExchangeOptions
     }
 }
 
 function EndPSSession {
-    Remove-PSSession $Session
+    if($Session) {
+        Remove-PSSession $Session
+    }
 }
 
 $run = $true
@@ -51,7 +84,7 @@ while($run) {
     Write-Host "--------------------------------"
     Write-Host ""
     Write-Host "Options"
-    Write-Host "1. Connect To Server"
+    Write-Host "1. Connect To Office365 Exchange"
     Write-Host "2. Quit"
 
     $option = Read-Host "What would you like to do? "
@@ -59,18 +92,20 @@ while($run) {
     if($option -eq "1") {
         $adminEmail = Read-Host "Office365 Admin"
         $adminPassword = Read-Host "Password" -AsSecureString
-        $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminEmail, $adminPassword
-        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credentials -Authentication Basic -AllowRedirection
         try {
+            $credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminEmail, $adminPassword
+            $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credentials -Authentication Basic -AllowRedirection -ErrorAction Stop
+
             Import-PSSession $Session -DisableNameChecking
+            PresentExchangeOptions
         }
-        catch{
-            "Error: "
-            Write-Host $_
-            EndPSSession
+        catch {
+            Write-Host "====================================="
+            Write-Host "Error: " $_
+            Write-Host "====================================="
         }
 
-        PresentExchangeOptions
+        EndPSSession
     }
     if($option -eq "2") {
         $run = $false
---
+++ ImportText.ps1
@@ -0,0 +1,2 @@
+#Import Test - use import command and specify this file to test in exchange 365 context
+Get-Mailbox | select -last 1
